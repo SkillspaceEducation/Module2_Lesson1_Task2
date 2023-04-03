e75c1f05e6be704ab3ed249bad47dd67ed5c7abe
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-04-03 08:44:41 +0300

    Создайте класс, содержащий два поля типа int - числитель и знаменатель обыкновенной дроби.
    ○ Конструктор класса должен инициализировать эти два поля.
    ○ Создайте метод класса, который будет выводить дробь в текстовой строке в формате x / y;
    ○ Создайте метод, умножающий текущую дробь на число типа double, переданное методу в параметре и возвращающий дробь -
    результат умножения;
    ○ Создайте метод, делящий текущую дробь на число типа double, переданное ему в параметре и возвращающий дробь -
    результат деления.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+/*
+Текст задания в файле src.Текст задания.txt
+ */
+class Fraction {
+    int numerat, denumerat;
+
+    Fraction(int numerat, int denumerat) {
+        this.numerat = numerat;
+        this.denumerat = denumerat;
+    }
+
+    static void fraction(int numerat, int denumrat) {
+        int n = gcdByEuclidsAlgorihm(numerat, denumrat);
+        System.out.println(numerat / n + " / " + denumrat / n);
+    }
+
+    static int gcdByEuclidsAlgorihm(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+        return gcdByEuclidsAlgorihm(n2, n1 % n2);
+    }
+}
+
 public class Main {
+
+    static int getDenumerateDouble(double num, int step) {
+        double doub = num * step; // смещаем дробную часть к целой
+        return (int) doub % step; // отбрасываем исходную целую часть
+    }
+
+    private static int getStep(double num) {
+        String[] array = String.valueOf(num).split("[.]"); // разбиваем число с палавающей точкой на массив
+        int len = array[1].length(); // определяем количество цифр в дробной части
+        return (int) Math.pow(10, len); // определяем коэффициент для смещения
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int denumerateDouble; // знаменатель числа с плавающей точкой
+        int step; // шаг для смещения дробной части числа с плавающей точкой
+        Fraction fract = new Fraction(20, 10);
+        Fraction.fraction(fract.numerat, fract.denumerat);
+        double f = 45.67;
+        int d = (int) f; // отбрасываем дробную часть с плаващей точкой
+        if (d == f) {
+            denumerateDouble = 0;
+            step = 1;
+        } else {
+            step = getStep(f);
+            denumerateDouble = getDenumerateDouble(f, step);
+        }
+        System.out.println(step);
+        System.out.println(denumerateDouble);
+
     }
 }
